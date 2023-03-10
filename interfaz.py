a2b9b2b21a3365ef56da5b59875bfa1c8d423abe
Gabriel Garcia <suissigkeiten@gmail.com>
2023-03-09 22:28:04 -0500

    Feature/Subscripciones

--- interfaz.py
+++ interfaz.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+from tkinter import ttk
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
+#import pandas as pd
 
 class Canal:
-    def __init__(self, canal, categoria="No categorizado"):
+    def _init_(self, canal, categoria="No categorizado"):
         self.canal = canal
         self.categoria = categoria
         
@@ -17,6 +19,8 @@ def indicate(lb,page):
 def hide_indicate():
     home_button_indicate.config(bg="#c3c3c3")
     canales_button_indicate.config(bg="#c3c3c3")
+    suscribirse_button_indicate.config(bg="#c3c3c3")
+    cerrar_button_indicate.config(bg="#c3c3c3")
 
 def home():
     home_frame = tk.Frame(main_frame)
@@ -24,7 +28,57 @@ def home():
     lb.pack()
     home_frame.pack(pady=20)
 
+
 def canales():
+    def obtener_canales_suscritos(yt):
+        # Obtener los canales a los que está suscrito el usuario
+        canales = []
+        request = yt.subscriptions().list(part="snippet", mine=True, maxResults=50)
+        print(request)
+        
+        while request is not None:
+            response = request.execute()
+            for item in response['items']:
+                canal = item['snippet']['title']
+                canales.append(Canal(canal))
+            request = yt.subscriptions().list_next(request, response)
+        return canales
+
+    canales_frame = tk.Frame(main_frame)
+    lb = tk.Label(canales_frame, text="Apartado Canales", font=("Bold", 30))
+    lb.pack()
+
+    # Obtener los canales a los que está suscrito el usuario como objetos Canal
+    canales = obtener_canales_suscritos(youtube)
+
+    # Crear una tabla para mostrar los canales
+    tabla = ttk.Treeview(canales_frame, columns=("Canal", "Categoría"), show="headings")
+    tabla.heading("Canal", text="Canal")
+    tabla.heading("Categoría", text="Categoría")
+    tabla.pack()
+
+    # Recorrer la lista de objetos Canal y mostrarlos en la tabla
+    for i, canal in enumerate(canales):
+        tabla.insert("", "end", values=(canal.canal, canal.categoria))
+
+    canales_frame.pack(pady=20)
+
+'''
+def canales():
+    def obtener_canales_suscritos(yt):
+        # Obtener los canales a los que está suscrito el usuario
+        canales = []
+        request = yt.subscriptions().list(part="snippet", mine=True, maxResults=50)
+        print(request)
+        
+        while request is not None:
+            response = request.execute()
+            for item in response['items']:
+                canal = item['snippet']['title']
+                canales.append(Canal(canal))
+            request = yt.subscriptions().list_next(request, response)
+        return canales
+
     canales_frame = tk.Frame(main_frame)
     lb=tk.Label(canales_frame,text="Apartado Canales",font=("Bold",30))
     lb.pack()
@@ -33,7 +87,7 @@ def canales():
     canales = obtener_canales_suscritos(youtube)
 
     # Crear una tabla para mostrar los canales
-    tabla_canvas = tk.Canvas(canales_frame)
+    tabla_canvas = tk.Canvas(canales_frame,width=400, height=350)
     tabla_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
     scrollbar = tk.Scrollbar(canales_frame, orient=tk.VERTICAL, command=tabla_canvas.yview)
@@ -58,30 +112,60 @@ def canales():
     tabla_canvas.config(scrollregion=tabla_canvas.bbox('all'))
 
     canales_frame.pack(pady=20)
+'''
 
-def obtener_canales_suscritos(yt):
-    # Obtener los canales a los que está suscrito el usuario como objetos Canal
-    canales = []
-    request = yt.subscriptions().list(part="snippet", mine=True, maxResults=50)
-    cuadro_texto = tk.Text(main_frame, height=30, width=50)
-    cuadro_texto.pack()
-    canales = obtener_canales_suscritos(youtube)
-    for canal in canales:
-        cuadro_texto.insert(tk.END, canal + "\n")
-
-def obtener_canales_suscritos(yt):
-    # Obtener los canales a los que está suscrito el usuario
-    canales = []
-    request = yt.subscriptions().list(part="snippet", mine=True, maxResults=50)
-    print(request)
-    
-    while request is not None:
+def suscribirse():
+    def search_channels(query):
+        request = youtube.search().list(
+            part='id,snippet',
+            q=query,
+            type='channel',
+            maxResults=30
+        )
         response = request.execute()
+
+        # Extrae la información relevante de la respuesta de la API de YouTube
+        channels = []
         for item in response['items']:
-            canal = item['snippet']['title']
-            canales.append(Canal(canal))
-        request = yt.subscriptions().list_next(request, response)
-    return canales
+            channel_id = item['id']['channelId']
+            channel_title = item['snippet']['title']
+            channel_description = item['snippet']['description']
+            channels.append({'Título': channel_title, 'Descripción': channel_description, 'ID':channel_id})
+
+        # Devuelve los resultados de búsqueda en forma de DataFrame de pandas
+        return pd.DataFrame(channels)
+    
+    def create_table(parent, df):
+        tree = ttk.Treeview(parent, columns=list(df.columns), show="headings")
+        for col in df.columns:
+            tree.heading(col, text=col)
+        for row in df.to_numpy().tolist():
+            tree.insert("", "end", values=row)
+        tree.pack(side='left', fill='both', expand=True,pady=20)
+        return tree
+    
+    def on_search():
+        query = search_entry.get()
+        df = search_channels(query)
+        create_table(table_frame, df)
+
+    suscribirse_frame=tk.Frame(main_frame)
+    lb=tk.Label(suscribirse_frame,text="Apartado suscribirse",font=("Bold",30))
+    lb.pack()
+
+    search_label = tk.Label(suscribirse_frame, text="Término de búsqueda:")
+    search_label.pack(side= 'left',padx=5, pady=20)
+
+    search_entry = tk.Entry(suscribirse_frame)
+    search_entry.pack(side='left', fill='x', padx=5, pady=20, expand=True)
+
+    search_button = tk.Button(suscribirse_frame, text="Buscar", command=on_search)
+    search_button.pack(side='left', padx=5, pady=20)
+
+    table_frame = tk.Frame(suscribirse_frame)
+    table_frame.pack(fill='both', expand=True,pady=20)
+
+    suscribirse_frame.pack(pady=20)
 
 def limpiarFrame():
     for frame in main_frame.winfo_children():
@@ -105,19 +189,24 @@ root.title("Nombre app")
 options_frame = tk.Frame(root, bg="#c3c3c3")
 
 home_button=tk.Button(options_frame,text="Home",font=("bold",15),fg="#158aff",bd=0,bg="#c3c3c3",command=lambda: indicate(home_button_indicate,home))
-home_button.place(x=10,y=50)
+home_button.place(x=8,y=50)
 home_button_indicate=tk.Label(options_frame,text="",bg="#158aff")
 home_button_indicate.place(x=3,y=50,width=5,height=40)
 
 canales_button=tk.Button(options_frame,text="Canales",font=("bold",15),fg="#158aff",bd=0,bg="#c3c3c3",command=lambda: indicate(canales_button_indicate,canales))
-canales_button.place(x=10,y=100)
+canales_button.place(x=8,y=100)
 canales_button_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
 canales_button_indicate.place(x=3,y=100,width=5,height=40)
 
+suscribirse_button=tk.Button(options_frame,text="Suscribir",font=("bold",15),fg="#158aff",bd=0,bg="#c3c3c3",command=lambda: indicate(suscribirse_button_indicate,suscribirse))
+suscribirse_button.place(x=8,y=150)
+suscribirse_button_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
+suscribirse_button_indicate.place(x=3,y=150,width=5,height=40)
+
 cerrar_button=tk.Button(options_frame,text="Cerrar",font=("bold",15),fg="#158aff",bd=0,bg="#c3c3c3",command=root.quit)
-cerrar_button.place(x=10,y=150)
+cerrar_button.place(x=8,y=200)
 cerrar_button_indicate=tk.Label(options_frame,text="",bg="#c3c3c3")
-cerrar_button_indicate.place(x=3,y=150,width=5,height=40)
+cerrar_button_indicate.place(x=3,y=200,width=5,height=40)
 
 options_frame.pack(side=tk.LEFT)
 options_frame.pack_propagate(False)
